144010de563b19c171d339d0e9477de40bffc85b
Sharmila Prabhu <sharmilag.91@gmail.com>
2022-12-01 21:49:36 +0530

    input field in table

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 
-var biscuit1 = {productName:"Mariegold", productId:"1001",price:100};
-var biscuit2 = {productName:"JimJam", productId:"1002",price:102};
-var biscuit3 = {productName:"Hide&Seek", productId:"1003",price:150};
-var biscuit4 = {productName:"Bourborn", productId:"1004",price:160};
+var biscuit1 = {productName:"Mariegold", productId:"1001",price:100,quantity:"<input type='number' value='1' class= box1>"};
+var biscuit2 = {productName:"JimJam", productId:"1002",price:102,quantity:"<input type='number' value='1' class= box2>"};
+var biscuit3 = {productName:"Hide&Seek", productId:"1003",price:150,quantity:"<input type='number' value='1' class= box3>"};
+var biscuit4 = {productName:"Bourborn", productId:"1004",price:160,quantity:"<input type='number' value='1' class= box4>"};
 
 
 tableData();
+
 function tableData()
 {
 var x = document.createElement("TABLE");
@@ -16,22 +17,22 @@ tableThead();
 var tr1 = document.createElement("TR");
 tr1.setAttribute("id","mytr1");
 document.getElementById("mytable").appendChild(tr1);
-tableBody(biscuit1,"mytr1");
+tableBody(biscuit1,"mytr1", "box1");
 
 var tr2 = document.createElement("TR");
 tr2.setAttribute("id","mytr2");
 document.getElementById("mytable").appendChild(tr2);
-tableBody(biscuit2,"mytr2");
+tableBody(biscuit2,"mytr2", "box2");
 
 var tr3 = document.createElement("TR");
 tr3.setAttribute("id","mytr3");
 document.getElementById("mytable").appendChild(tr3);
-tableBody(biscuit3,"mytr3");
+tableBody(biscuit3,"mytr3", "box3");
 
 var tr4 = document.createElement("TR");
 tr4.setAttribute("id","mytr4");
 document.getElementById("mytable").appendChild(tr4);
-tableBody(biscuit4,"mytr4");
+tableBody(biscuit4,"mytr4","box4");
 }
 
 
@@ -79,41 +80,44 @@ function tableThead(){
 }
 
 
-function tableBody(objectname, idname){
+function tableBody(objectname, idname, cname){
 	
 	var td1=document.createElement("TD");
-	var node1 = document.createTextNode(objectname.productName);
-	td1.appendChild(node1);
+	td1.innerHTML=objectname.productName;
 	document.getElementById(idname).appendChild(td1);
 	
 	var td2=document.createElement("TD");
-	var node2 = document.createTextNode(objectname.productId);
-	td2.appendChild(node2);
+	td2.innerHTML=objectname.productId
 	document.getElementById(idname).appendChild(td2);
 	
 	var td3=document.createElement("TD");
 	var node3 = document.createTextNode(objectname.price);
-	td3.appendChild(node3);
+	td3.innerHTML= objectname.price;
 	document.getElementById(idname).appendChild(td3);
 	
 	var td4=document.createElement("TD");
-	txt=textbox();
-	var node4 = document.createTextNode(txt);
-	td4.appendChild(node4);
+	var ch=objectname.quantity;
+	td4.innerHTML= ch;
 	document.getElementById(idname).appendChild(td4);
 	
 	var td5=document.createElement("TD");
-	var node5 = document.createTextNode("500");
-	td5.appendChild(node5);
+	var table = document.getElementById("mytable");
+	var item=table.getElementsByClassName(cname)[0].value;
+	var node5 = item * objectname.price;
+	console.log(item);
+	td5.innerHTML = node5;
 	document.getElementById(idname).appendChild(td5);
 	}
 	
 
 function textbox(){
-	var y = document.createElement("INPUT");
-	y.setAttribute("type","number");
-	y.setAttribute("value","1");
-	y.setAttribute("id", "box");
+	let box;
+	var z;
+	for(i=1;i<=4;i++){
+	box='box'+i;
+	z="<input type='number' value='1' class= box>";
+	}
 	
-	return y;
+	return "<input type='number' value='1' class= box>";
 }
+
